695efcfe7f8b943d8bac5bf6a4c81be9adeb1925
Omid Ghavami <93787288+OmidGhavamii@users.noreply.github.com>
2022-03-02 10:40:14 +0330

    Update HomeWork-2.java

--- L0/2/HomeWork-2.java
+++ L0/2/HomeWork-2.java
@@ -1,1 +1,32 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                Scanner input = new Scanner(System.in);
+                System.out.println("Enter Entegers=");
+                int num1 = input.nextInt();
+                int num2 = input.nextInt();
+                int num3 = input.nextInt();
+
+
+        if(num1 >= num2) {
+
+            if(num1 >= num3)
+                System.out.println(num1+" is the largest Number");
+            else
+                System.out.println(num3+" is the largest Number");
+        }
+        else {
+
+            if (num2 >= num3)
+                System.out.println(num2 + " is the largest Number");
+            else
+                System.out.println(num3 + " is the largest Number");
+        }
+    }
+}
 
