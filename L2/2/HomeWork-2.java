8c40dcbfdce9caa784cb624b167164f394f584f4
Omid Ghavami <93787288+OmidGhavamii@users.noreply.github.com>
2022-03-17 00:46:42 +0330

    Create HomeWork-2.java

---
+++ L2/2/HomeWork-2.java
@@ -0,0 +1,20 @@
+package project;
+import java.util.Scanner;
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        string a;
+        System.out.print("Enter text: ");
+        String myString = input.next();
+        System.out.println("Text entered = " + myString);
+        if (myString == "BarnameSaziPishrafte") ;
+        {
+            System.out.println("Pishniaz = Mabani Barname nevisi\nTedad Vahed = 3\nHamniaz = Kargah Computer");
+        }
+
+        }
+    }
+
+
